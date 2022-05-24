849186313f4cb25ca3a1d1873646c30b559e93d9
Kirill Zagul <zagulkirill@gmail.com>
2022-05-24 21:02:05 +0500

    home page updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,9 +7,9 @@
       <div class="d-flex align-center">
         <router-link :to="'/'" exact>
           <v-img
+            contain
             alt="Vuetify Logo"
             class="shrink mr-2"
-            contain
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/1C_Company_logo.svg/2560px-1C_Company_logo.svg.png"
             transition="scale-transition"
             width="40"
@@ -19,11 +19,7 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn text>
         <v-icon color="deep-orange accent-3">mdi-basket</v-icon>
       </v-btn>
     </v-app-bar>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,12 +5,12 @@
             <v-layout justify-center align-center>
                 <v-flex shrink>
                     <div class="pa-10 pb-8 pt-10 text-center">
-                        <h1 class="pb-5">Добро пожаловать на портал о ебанном<span class="red--text text--darken-4"><i>1С</i></span></h1>
+                        <h1 class="pb-5">Добро пожаловать на веб-сайт по <span class="red--text text--darken-4"><i>1С</i></span></h1>
                         <v-divider></v-divider>
                         <div class="pt-5">
                             <router-link to="/table" exact style="text-decoration: none; color: inherit;">
                                 <v-btn v-bind="attrs" v-on="on" class="ma-1" large color="#BDBDBD">
-                                    <h3>В ебеня</h3>
+                                    <h3>Перейти к таблице</h3>
                                 </v-btn>
                             </router-link>
                         </div>
